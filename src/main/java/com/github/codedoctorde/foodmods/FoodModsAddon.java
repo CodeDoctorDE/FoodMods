ea84fe9b641505b4ee4e887b2de914a02ba27e88
CodeDoctorDE <wusel905@gmail.com>
2020-09-08 21:28:50 +0200

    register item template

--- src/main/java/com/github/codedoctorde/foodmods/FoodModsAddon.java
+++ src/main/java/com/github/codedoctorde/foodmods/FoodModsAddon.java
@@ -1,5 +1,6 @@
 package com.github.codedoctorde.foodmods;
 
+import com.github.codedoctorde.foodmods.template.FoodTemplate;
 import com.github.codedoctorde.itemmods.api.ItemModsAddon;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -8,6 +9,10 @@ import org.jetbrains.annotations.Nullable;
  * @author CodeDoctorDE
  */
 public class FoodModsAddon extends ItemModsAddon {
+    public FoodModsAddon(){
+        registerItemTemplate(new FoodTemplate());
+    }
+
     @NotNull
     public String getName() {
         return "Example Name";
